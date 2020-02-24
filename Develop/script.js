c19fe70996be876039ef76a53fe41600132a67cf
naritchaya louis <naritchayalouis@gmail.com>
2020-02-24 01:39:09 -0600

    added generate password button, prompts, confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,41 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var generateBtn = document.getElementById("generate")
+console.log(generateBtn);
+generateBtn.addEventListener("click", writePassword)
+  
+  
+
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+  var charAmount = prompt("How many chracters do you want? ")
+  if( charAmount <8 || charAmount >128) {
+    alert("Password must include a minimum of 8 characters and no more than 128 characters.");
+   
+    if (charAmount) {
+      prompt("How many chracters do you want? ")
+    }
+  }
 
-  passwordText.value = password;
+  var charLow = confirm("Do you want lowercase characters?")
 
+  var charUp = confirm("Do you want uppercase characters? ")
+
+  var charNum = confirm("Do you want numberic characters? ")
+
+  var charSpecial = confirm("Do you want special characters? ")
+    
+
+  var passwordCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@!%&()/";
+  passwordValue = "";
+  for ( var i = 0, n = passwordCharset.length; i < charAmount; ++i) {
+   passwordValue += passwordCharset.charAt(Math.floor(Math.random() * n ));
+  }
+
+  document.getElementById("password").value = passwordValue;
+ 
+ return;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-
-var loginConfirm1 = confirm(" I need a new, secure password?"); {
-
-
-
-
-
-}
